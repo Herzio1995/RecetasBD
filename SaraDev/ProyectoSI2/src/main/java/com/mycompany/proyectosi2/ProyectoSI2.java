f97aede9802c9a3b7ca24094352f366a0973b408
Sara Colque  Guzman <saracolque1234@gmail.com>
2023-03-31 22:36:03 -0400

    Inicial Commit

---
+++ SaraDev/ProyectoSI2/src/main/java/com/mycompany/proyectosi2/ProyectoSI2.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectosi2;
+
+/**
+ *
+ * @author HP VICTUS
+ */
+public class ProyectoSI2 {
+
+    public static void main(String[] args) {
+       CRUD objetoCRUD = new CRUD();
+       objetoCRUD.setVisible(true);
+    }
+}
