833bfcae691c4c5a882bdd6fcf19791bcf7efb7e
RocioDelAlb4 <105135061+RocioDelAlb4@users.noreply.github.com>
2023-04-02 21:22:48 -0400

    US 13

--- AppEclipse/src/Aplicacion/universalDocRenderer.java
+++ AppEclipse/src/Aplicacion/universalDocRenderer.java
@@ -1,5 +1,27 @@
 package Aplicacion;
 
-public class universalDocRenderer {
+import java.io.FileOutputStream;
 
+import com.itextpdf.text.Document;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
+
+
+public class universalDocRenderer {
+	public void renderizar(HistoriaDeUsuario historia, String ruta) throws Exception {
+        Document documento = new Document();
+        PdfWriter.getInstance(documento, new FileOutputStream(ruta));
+
+        documento.open();
+        documento.add(new Paragraph("Título: " + historia.getTitulo()));
+        documento.add(new Paragraph("ID: " + historia.getID()));
+        documento.add(new Paragraph("Yo: " + historia.getYo()));
+        documento.add(new Paragraph("Requiero: " + historia.getRequiero()));
+        documento.add(new Paragraph("Tal que: " + historia.getTalQue()));
+        documento.add(new Paragraph("Condiciones de aceptacion: " + historia.getCondicionesDeAceptacion()));
+        documento.add(new Paragraph("Complejidad: " + historia.getComplejidad()));
+        documento.add(new Paragraph("Importancia: " + historia.getImportancia()));
+
+        documento.close();
+    }
 }
